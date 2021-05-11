df8d789772c8cd6424431a150857b6919991cc4c
unknown <alejandrojzeballos@gmail.com>
2021-05-11 18:16:39 -0400

    {add} creando proyecto con react con create-react-app

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -3,4 +3,15 @@
     node 12
     npm 6 
 
+    create React App 
+    crea el proyecto en react
+    Crea un entorno de desarrollo, es una herramienta para ejecutar nuestras app react
+    el requerimiento es solo tener nodejs
+
+    npx no es otro gestro de paquetes si no es un ejecutor de paquetes binarios de npm otro CLI
+
+    npx instala de manera local busca las depedendencias en nodemodules
+
+    
+
 */
